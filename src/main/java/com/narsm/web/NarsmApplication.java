bc10a145c574e5d5e5668b38c0c5879a4e5d703c
minho <joo25sk@naver.com>
2022-10-17 15:51:38 +0900

    Created Narsm Project

---
+++ src/main/java/com/narsm/web/NarsmApplication.java
@@ -0,0 +1,13 @@
+package com.narsm.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarsmApplication.class, args);
+	}
+
+}
